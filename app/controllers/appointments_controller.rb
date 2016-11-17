2280eabf47d40d46f761773ce4d607c4240ad6e2
Mattycodes <matt.nick.fagioli@gmail.com>
2016-11-16 18:01:15 -0600

    Implements initial appointment-making functionality.

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,50 @@
+class AppointmentsController < ApplicationController
+  # GET /appointments
+  # GET /appointments.json
+  def index
+    @appointments = Appointment.all
+  end
+
+  # GET /appointments/1
+  # GET /appointments/1.json
+  def show
+  end
+
+  # GET /appointments/new
+  def new
+    @appointment = Appointment.new
+  end
+
+  # GET /appointments/1/edit
+  def edit
+  end
+
+  # POST /appointments
+  # POST /appointments.json
+  def create
+    @appointment = Appointment.new
+    @appointment.topic = params[:appointment][:topic]
+    @appointment.start_time = @appointment.generate_start_time(params)
+    @appointment.end_time = @appointment.generate_end_time(params)
+    @appointment.interval = params[:appointment][:interval].to_i
+    if @appointment.save
+      redirect_to "/"
+    else
+      render "new"
+    end
+  end
+
+  # PATCH/PUT /appointments/1
+  # PATCH/PUT /appointments/1.json
+  def update
+
+
+  end
+
+  # DELETE /appointments/1
+  # DELETE /appointments/1.json
+  def destroy
+    
+  end
+
+end
---
+++ app/models/appointment.rb
@@ -0,0 +1,29 @@
+class Appointment < ApplicationRecord
+	# belongs_to :user
+	validates :start_time, presence: true
+	validates :topic, presence: true
+	validates :interval, presence: true
+
+	def generate_start_time(params)
+		date = params[:date].split('/')
+		year = date[2].to_i
+		month = date[0].to_i
+		day = date[1].to_i
+		time = params[:start_time].split(':')
+		if time[1][-2..-1].downcase == 'pm'
+			hour = time[0].to_i + 12
+		elsif time[1][-2..-1].downcase == 'am'
+			hour = time[0].to_i
+		else
+			nil
+		end
+		time[1].slice!(-2..-1)
+		minute = time[1].to_i
+		Time.new(year, month, day, hour, minute)
+	end
+
+	def generate_end_time(params)
+		start_time = self.generate_start_time(params)
+		start_time + ( params[:appointment][:interval].to_i * 60 )
+	end
+end
